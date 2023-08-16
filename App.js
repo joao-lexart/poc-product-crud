6a54559bea26591bde6ab744ae8f63095c97141f
Joao Vargas <joao.vargas@lexartlabs.xyz>
2023-08-16 09:34:06 -0300

    style: add classes to itens

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, Text, View, TextInput, FlatList, Pressable } from 'react-native';
 import { collection, addDoc, query, getDocs } from "firebase/firestore"; 
 import { db } from './components/config';
@@ -10,7 +10,28 @@ export default function App() {
   const [category, setCategory] = useState('');
   const [price, setPrice] = useState('');
   const [currency, setCurrency] = useState('');
-  const [products, setProducts] = useState([]);
+  const [productList, setProductList] = useState([]);
+
+
+  const getProducts = useCallback( async () => {
+    console.log("entrou no useCallback")
+    const productsRef = collection(db, 'products');
+    const q = query(productsRef);
+    const products = [];
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      const { name, category, price, currency} = doc.data();
+      products.push({
+        id: doc.id,
+        name, 
+        category,
+        price,
+        currency,
+      });
+    });
+    setProductList(products)
+  }, [productList]) 
 
   function create () {
     addDoc(collection(db, "products"), {
@@ -23,27 +44,9 @@ export default function App() {
     }).catch((err) => {
       console.log(err);
     });
+    getProducts();
   }
 
-  async function getProducts () {
-    const productsRef = collection(db, 'products');
-    const q = query(productsRef);
-    const products = [];
-    const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-      const { name, category, price, currency} = doc.data();
-          products.push({
-            id: doc.id,
-            name, 
-            category,
-            price,
-            currency,
-         });
-});
-  setProducts(products)
-  console.log(products);
-}
 
 useEffect(() => {
   getProducts()
@@ -62,15 +65,15 @@ useEffect(() => {
 
       <FlatList 
         style={{height: '100%'}}
-        data={products}
+        data={productList}
         numColumns = {1}
         renderItem = {({item}) => (
-          <Pressable style = {StyleSheet.container}>
-            <View style = {StyleSheet.innerContainer}>
-              <Text> Name: {name} </Text>
-              <Text> Category: {category} </Text>
-              <Text> Price: {price} </Text>
-              <Text> Currency: {currency} </Text>
+          <Pressable style = {styles.container}>
+            <View style = {styles.innerContainer}>
+              <Text style={styles.itemHeading}> Name: {item.name} </Text>
+              <Text style={styles.itemText}>  Category: {item.category} </Text>
+              <Text style={styles.itemText}> Price: {item.price} </Text>
+              <Text style={styles.itemText}> Currency: {item.currency} </Text>
             </View>
           </Pressable>
         )}
@@ -82,7 +85,11 @@ useEffect(() => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e5e5e5',
+    padding: 15,
+    borderRadius: 15,
+    margin: 5,
+    marginHorizontal: 10,
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -93,5 +100,15 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     borderWidth: 0.2,
     borderRadius: 30, 
+  },
+  innerContainer: {
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  itemHeading: {
+    fontWeight: 'bold',
+  },
+  itemText: {
+    fontWeight: '300',
   }
 });
